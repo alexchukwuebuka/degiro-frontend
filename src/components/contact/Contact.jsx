6e623b9f0503bf04cfd7102d8c88673bfe7b2820
trilvid <kelvinchukwuebuka385@gmail.com>
2026-01-29 14:44:28 +0100

    first commit

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -40,6 +40,9 @@ const Contact = () => {
                         <a href='mailto:degiromanagements@gmail.com' className="ball contact-ball" target='blank'>
                             <FiMail />
                         </a>
+                        <a href="tel:9455997111" className="ball contact-ball" target='blank'>
+                            <FaPhone />
+                        </a>
                         {/* <a href="https://api.whatsapp.com/send?phone=447529602972"className="ball contact-ball" target='blank'>
                     <BsWhatsapp />
                 </a> */}
