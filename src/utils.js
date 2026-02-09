1aaaeaeb71f6ba2c8e405bd3a4dc27f22e0bdf51
trilvid <kelvinchukwuebuka385@gmail.com>
2026-02-09 21:06:35 +0100

    updated

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
 export const formatCurrency = (amount) => {
-    if (amount === undefined || amount === null || isNaN(amount)) return '0.000';
-    return Number(amount).toLocaleString('en-US', { minimumFractionDigits: 3, maximumFractionDigits: 3 });
+    if (amount === undefined || amount === null || isNaN(amount)) return '0';
+    return Number(amount).toLocaleString('en-US', { maximumFractionDigits: 3 });
 };
